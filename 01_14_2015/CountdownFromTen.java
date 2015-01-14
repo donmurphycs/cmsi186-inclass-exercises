3de99ce1dc1c2abd7d3c01b7684d25533b26f09d
donmurphycs <don.murphy.cs@gmail.com>
2015-01-14 11:45:06 -0800

    Moved exercises/examples to dated folders

--- 01_14_2015/CountdownFromTen.java
+++ 01_14_2015/CountdownFromTen.java
@@ -1,6 +1,6 @@
-public class CountToTen {
+public class CountdownFromTen {
     public static void main(String[] args) {
-	    for (int i = 0; i <= 10; i++) {
+	    for (int i = 10; i >= 0; i--) {
 		    System.out.printf(i + " ");
 		}
 	}
