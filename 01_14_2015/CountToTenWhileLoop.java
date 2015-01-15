bda1da7be39fb74d47246d073cff3aa54c6822dd
donmurphycs <donmurphycs@users.noreply.github.com>
2015-01-15 14:26:57 -0800

    The right indent correction

--- 01_14_2015/CountToTenWhileLoop.java
+++ 01_14_2015/CountToTenWhileLoop.java
@@ -1,9 +1,9 @@
 public class CountToTenWhileLoop {
     public static void main(String[] args) {
 	int i = 0;
-	    while (i <= 10) {
-		    System.out.printf(i + " ");
-			i++;
-		}
-	}
+	while (i <= 10) {
+		System.out.printf(i + " ");
+		i++;
+        }
+    }
 }
